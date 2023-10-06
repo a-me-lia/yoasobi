69b60df661b1d59d3f8a11cf2c2c4d1cd60f0fd2
matthewguo86 <110259342+a-me-lia@users.noreply.github.com>
2023-10-06 12:25:52 -0400

    dynamic height
    Signed-off-by: matthewguo86 <110259342+a-me-lia@users.noreply.github.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
 <main>
-    <div className='w-full h-screen relative'>
+    <div className='w-full h-[calc(100dvh)] relative'>
       <Image src='/ruby.avif' alt='ruby' fill className='object-cover'></Image>
     </div>
 </main>
