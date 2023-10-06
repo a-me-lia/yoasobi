534ad756d6f14644350c551226111aefc9a9ac55
Marin Kurokawa <110259342+a-me-lia@users.noreply.github.com>
2023-10-06 13:55:30 -0400

    Update page.tsx
    Image displays

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,9 @@ import SignUpButton from './components/signupbutton'
 export default function Home() {
   return (
 <main className='h-[calc(100dvh)]'>
-    {/* <div className='w-full h-full absolute z-0'>
+    <div className='w-full h-full absolute z-0'>
       <Image src='/ruby.avif' alt='ruby' fill className='object-cover'></Image>
-    </div> */}
+    </div>
 
     <div className='relative flex flex-col items-center justify-center z-50 h-full space-y-16'>
       <h1 className='font-mono md:text-8xl text-4xl font-medium text-white drop-shadow-2xl '>Coming Soon</h1>
